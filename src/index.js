5dbacec0f273bcb6426fa2f654fc750615a5d7f5
anupandey <[your email]>
2018-12-07 18:49:13 -0800

    changes to leaderboard.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import firebase from './firebase.js'
 //     let display = document.querySelector('#timer-area');
 //     setInterval(function () {
 //         display.textContent = '';
-//         }, 10);        
+//         }, 10);
 // }
 
 class Timer extends React.Component{
@@ -30,7 +30,7 @@ class Timer extends React.Component{
         }
         return(<div></div>);
     }
-    
+
 }
 class App extends React.Component {
   // This component contains the entirety of your application
@@ -43,7 +43,7 @@ class App extends React.Component {
   }
   pageNav(pageid){
    // console.log('App '+pageId);
-    this.setState((state) => ({pageId :pageid, userName : state.username}));    
+    this.setState((state) => ({pageId :pageid, userName : state.username}));
     //console.log(this.state.navPage);
     // this.child.pageNav(pageId);
     // this.child3.pageNav(pageId);
@@ -61,13 +61,13 @@ class App extends React.Component {
             console.log(c_arr[this.state.userName]);
             this.userScore = c_arr[this.state.userName].score;
         }
-            
+
     }).then(()=>{
         if(scr > this.userScore || this.userScore == 0){
             rsvpRef.child(this.state.userName).set({'name':this.state.userName,'score':scr});
         }
     })
-      this.setState((state) =>{return{userName:state.userName, pageId:3, score:scr }});      
+      this.setState((state) =>{return{userName:state.userName, pageId:3, score:scr }});
   }
   render() {
     if(this.state.pageId==1){
@@ -83,7 +83,6 @@ class App extends React.Component {
         );
     }
     else if(this.state.pageId == 3){
-
         return(
             <div>
             <Display userName = {this.state.userName} score = {this.state.score}/>
@@ -105,8 +104,7 @@ class App extends React.Component {
 ReactDOM.render(
   <App pageId={1} />,
   document.getElementById('game-area'));
-  
+
   ReactDOM.render(
     <Timer disp_state = {false}></Timer>,
     document.getElementById('timer-area'));
-
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,31 +1,66 @@
 import React from "react";
 import firebase from './firebase.js';
 
+class LeaderBoardItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>
+        {this.props.name} has score {this.props.score}
+      </li>
+    );
+  }
+}
+
 class Display extends React.Component{
     constructor(props){
         super(props);
-        this.leaderBoard = []
+        this.state = {
+          leaderBoard : []
+        };
     }
+
+    componentDidMount(){
+      const rsvpRef = firebase.database().ref('scores').orderByChild('score').limitToLast(5);
+      let leaderBoard = [];
+      rsvpRef.once('value').then((snapshot)=>{
+          let c_arr=snapshot.val();
+          for (let e in c_arr){
+            leaderBoard.push(c_arr[e]);
+          }
+
+          leaderBoard.sort(function(a, b) {
+              var keyA = a.score,
+                  keyB = b.score;
+              if(keyA < keyB) return 1;
+              if(keyA > keyB) return -1;
+              return 0;
+          });
+
+          this.setState({
+            leaderBoard : leaderBoard
+          });
+      });
+    }
+
     render(){
-        const rsvpRef = firebase.database().ref('scores').orderByChild('score').limitToLast(5);
-        rsvpRef.once('value').then((snapshot)=>{
-            let c_arr=snapshot.val();
-            for (let e in c_arr){
-                this.leaderBoard.push([e,c_arr[e]]);
-            }
-            this.leaderBoard.sort(function(a, b) {
-                return a.score > b.score;
-            });  
-            this.leaderBoard = this.leaderBoard.sort();          
-            //console.log('c_arr',c_arr)
-        })
-        console.log(this.leaderBoard);
+        console.log('Leaderboard render',this.state.leaderBoard);
+        const leaderBoardItems = this.state.leaderBoard.map((leader) =>
+        <LeaderBoardItem name={leader.name} score={leader.score} />
+        );
         return(
             <div className=".body">
                 <h1>Congratulations {this.props.userName} !!!!</h1>
-                <h2> You Scored {this.props.score}</h2>
+                <h2>You Scored {this.props.score}</h2>
+                <h2>Cuiz Leaderboard (Top 5 Scores)</h2>
+                <ul>
+                  {leaderBoardItems}
+                </ul>
             </div>
-        )
+        );
     }
 }
 
